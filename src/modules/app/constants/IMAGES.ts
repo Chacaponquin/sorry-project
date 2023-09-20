6dfa1b5b612c89187406a476247ccb98fa8c1bea
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-20 02:35:40 -0400

    Optimize images

--- src/modules/app/constants/IMAGES.ts
+++ src/modules/app/constants/IMAGES.ts
@@ -1,6 +1,6 @@
 export const APP_IMAGES = {
-  AVATAR: { image: "/images/amaya_avatar.png", alt: "Amaya Avatar" },
-  FORGIVE: { image: "/images/forgive.png", alt: "Forgive Picture" },
-  CAR_GLASSES: { image: "/images/cat_glasses.jpg", alt: "Cat With Glasses" },
-  PROGRAMMER: { image: "/images/programmer.jpg", alt: "Programmer" },
+  AVATAR: { image: "/images/amaya_avatar.webp", alt: "Amaya Avatar" },
+  FORGIVE: { image: "/images/forgive.webp", alt: "Forgive Picture" },
+  CAR_GLASSES: { image: "/images/cat_glasses.webp", alt: "Cat With Glasses" },
+  PROGRAMMER: { image: "/images/programmer.webp", alt: "Programmer" },
 } as const;
